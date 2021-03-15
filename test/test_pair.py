87903503678fde2d331193637b00ad26f673adcd
bqx619 <bqx619@gmail.com>
2021-03-15 20:16:46 +0800

    feat: initialize the EI pairing framework

---
+++ test/test_pair.py
@@ -0,0 +1,15 @@
+from core.pair import Pair, IID_KEY, SIZE_KEY, USER_KEY, CATEGORY_KEY
+
+
+def test_execute():
+    ins = Pair()
+    ins.set([
+        {SIZE_KEY: 0.5, IID_KEY: '1', USER_KEY: 'Bob', CATEGORY_KEY: 'ICP_A'},
+        {SIZE_KEY: 2, IID_KEY: '1', USER_KEY: 'Bob', CATEGORY_KEY: 'ICP_B'},
+        {SIZE_KEY: 1, IID_KEY: '1', USER_KEY: 'Lily', CATEGORY_KEY: 'ICP_A'},
+        {SIZE_KEY: 8, IID_KEY: '1', USER_KEY: 'Bob', CATEGORY_KEY: 'ICP_A'},
+        {SIZE_KEY: 4, IID_KEY: '1', USER_KEY: 'Lily', CATEGORY_KEY: 'ICP_B'},
+    ])
+    ret = ins.execute()
+    print(ret)
+    assert len(ret) == 2
